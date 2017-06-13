21a94caa4f3fa17a2c3c6628e2606fb22e1ae4a7
Thomas SAUVAJON <thomas.sauvajon@gmail.com>
2017-06-13 17:33:39 +0200

    UI pour portail

--- MySweetHome/script.js
+++ MySweetHome/script.js
@@ -1,29 +1,48 @@
-function ouvrePortail() {
-  $.post("ouvrePortail", { ouvre: true }, function(data) {
-    console.log(data);
-  });
-}
+var ip = "http://192.168.0.23/";
+
+// function ouvrePortail() {
+//   $.post("ouvrePortail", { ouvre: true }, function(data) {
+//     console.log(data);
+//   });
+// }
+//
+// function fermePortail() {
+//   $.post("ouvrePortail", { ouvre: false }, function(data) {
+//     console.log(data);
+//   });
+// }
 
 function fermePortail() {
-  $.post("ouvrePortail", { ouvre: false }, function(data) {
-    console.log(data);
-  });
+  console.log("ça devrait close");
+  $.get(ip + "?portal=close");
 }
 
-function isGarageOuvert() {
-  $.get("isGarageOuvert", function(data) {
-    $(".isGarageOuvert").html(data);
-  });
+function ouvrePortail() {
+  console.log("ça devrait oepn");
+  $.get(ip + "?portal=open");
 }
-
-function isMovement() {
-  $.get("isMovement", function(data) {
-    $(".isMovement").html(data);
-  });
-}
+//
+// function isGarageOuvert() {
+//   $.get("isGarageOuvert", function(data) {
+//     $(".isGarageOuvert").html(data);
+//   });
+// }
+//
+// function isMovement() {
+//   $.get("isMovement", function(data) {
+//     $(".isMovement").html(data);
+//   });
+// }
 
 $(function() {
   // init polling
-  setTimeout(isGarageOuvert, 300);
-  setTimeout(isMovement, 300);
+  // setTimeout(isGarageOuvert, 300);
+  // setTimeout(isMovement, 300);
+  // $.ajax({
+  //   url: ip + "?portal=close",
+  // });
+  // function ()
+  // $.get(ip + "?portal=open", function(data) {
+  //   console.log(data);
+  // });
 });
